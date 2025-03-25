027a9753b9c83a0d474be1eb4d0a5e6ce8836716
jejellyroll-fr <jejellyroll.fr@gmail.com>
2025-03-25 21:12:19 +0100

    lint, add mixin, resolve fixme

--- gui_elements.py
+++ gui_elements.py
@@ -1,43 +1,69 @@
 #!/usr/bin/env python3
 
-# gui_elements.py --- 
-# 
+# gui_elements.py ---
+#
 # Filename: gui_elements.py
-# Description: 
-# Author: Johann 
-# Maintainer: 
+# Description:
+# Author: Johann
+# Maintainer:
 # Created: Don Mar 17 10:51:40 2016 (+0100)
-# Version: 
-# Last-Updated: 
-#           By: 
-#     Update #: 0
-# URL: 
-# Keywords: 
-# Compatibility: 
-# 
-# 
+# Version:
+# Last-Updated:
+#           By: jejellyroll
+#     Update #: 1
+# URL:
+# Keywords:
+# Compatibility:
+#
+#
 
-# Commentary: 
+# Commentary:
 # Implements classes for gui subelements
-# 
-# FIXME improve range combination to avoid not needed ( ) etc
-# 
+#
+#
+#
 
 # Change Log:
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 
 
 # Code:
 
 
-from tkinter import *
-from tkinter import ttk
-from utils import *
-
 import logging
+from tkinter import END, BooleanVar, E, N, S, StringVar, W, ttk
+
+from utils import (
+    EV_PLAYER_FRAME_PADDING,
+    FONT_FAM,
+    FONT_SIZE,
+    FRAME_PADDING,
+    INPUT_LENGTH,
+    PADX,
+    PADY,
+    PRE_INPUT_LENTH,
+    SPACES_BEFORE_SUMMARY_LINE,
+    START_RANGE_WIDTH,
+)
+
+
+class RangeFormatterMixin:
+    @staticmethod
+    def add_parenthesis(range_string: str) -> str:
+        """Ajoute des parenthèses si la chaîne contient des séparateurs indiquant
+        une combinaison d’éléments et si ce n’est pas déjà encadré."""
+        if not range_string or len(range_string) < 3:
+            return range_string
+        if range_string.startswith('(') and range_string.endswith(')'):
+            return range_string
+        delimiters = [',', '!', ':', '+']
+        if any(d in range_string for d in delimiters):
+            return f"({range_string})"
+        return range_string
+
 
 class ScrolledTextLogger(logging.Handler):
     def __init__(self, widget):
@@ -55,7 +81,7 @@ class ScrolledTextLogger(logging.Handler):
         # This is necessary because we can't modify the Text from other threads
         self.widget.after(0, append)
 
-        
+
 class RangeLine:
     def __init__(self, master,  x_box=True, text_field_length=INPUT_LENGTH, freq=True, equity=True):
         column=0 # counter number of already created items
@@ -64,7 +90,7 @@ class RangeLine:
         self.is_equity=equity
         self.input_range=StringVar()
         self.master_frame=master
-        
+
         if self.is_x_box:
             self.x_box_value=BooleanVar()
             self.xbox=ttk.Checkbutton(self.master_frame, variable=self.x_box_value)
@@ -90,8 +116,9 @@ class RangeLine:
             self.equity_lable['textvariable'] = self.equity
             self.equity.set("0.0")
             column=column+1
-            
-        for element in self.master_frame.winfo_children(): element.grid_configure(padx=PADX, pady=PADY)
+
+        for element in self.master_frame.winfo_children():
+            element.grid_configure(padx=PADX, pady=PADY)
 
     def get_xbox(self):
         return self.x_box_value.get()
@@ -103,21 +130,21 @@ class RangeLine:
         if value < 0 or value > 100:
             self.freq.set("??.?")
         else:
-            self.freq.set("%.1f" % value)
+            self.freq.set(f"{value:.1f}")
         return
 
     def set_equity(self, value):
         if value < 0 or value > 100:
             self.equity.set("??.?")
         else:
-            self.equity.set("%.1f" % value)
-        return    
+            self.equity.set(f"{value:.1f}")
+        return
 
-class Range:
+class Range(RangeFormatterMixin):
     def __init__(self, master, title, start_range=" ", num_sub_ranges=4, text_field_length=INPUT_LENGTH, freq=True, equity=True, x_box=True):
         self.title_frame=ttk.Frame(master, padding=FRAME_PADDING)
         self.title_frame.grid(column=0, row=0, sticky=W)
-        
+
         self.title=ttk.Label(self.title_frame, text=title+":", width=6)
         self.title.grid(column=0,row=0, sticky=W)
 
@@ -136,7 +163,7 @@ class Range:
         self.range_eq_label.grid(column=4,row=0, sticky=W)
         self.range_eq=StringVar()
         self.range_eq_label['textvariable'] = self.range_eq
-        self.range_eq.set("0.0")       
+        self.range_eq.set("0.0")
 
         self.num_sub_ranges=num_sub_ranges
         self.sub_range_frames=[]
@@ -151,7 +178,7 @@ class Range:
 
         self.summary_title=ttk.Label(self.summary_frame, text=" "*SPACES_BEFORE_SUMMARY_LINE + "Selection Combined (Frequency Equity): ")
         self.summary_title.grid(column=0,row=0, sticky=W)
-        
+
         self.summary_freq_label=ttk.Label(self.summary_frame,width=5,anchor=E)
         self.summary_freq_label.grid(column=1,row=0, sticky=W)
         self.summary_freq=StringVar()
@@ -163,7 +190,7 @@ class Range:
         self.summary_eq_label.grid(column=3,row=0, sticky=W)
         self.summary_eq=StringVar()
         self.summary_eq_label['textvariable'] = self.summary_eq
-        self.summary_eq.set("0.0")      
+        self.summary_eq.set("0.0")
 
     def get_xbox(self, index):
         if index >= len(self.sub_range_list):
@@ -176,10 +203,10 @@ class Range:
             self.start_range.set(range_string[:START_RANGE_WIDTH-4]+"...")
         else:
             self.start_range.set(range_string)
-            
+
     def get_start_range(self):
         return self.start_range_full
-    
+
     def set_freq(self, index, value):
         if index >= len(self.sub_range_list):
             return
@@ -189,49 +216,41 @@ class Range:
         if value < 0 or value > 100:
             self.summary_freq.set("??.?")
         else:
-            self.summary_freq.set("%.1f" % value)
+            self.summary_freq.set(f"{value:.1f}")
         return
 
     def set_equity(self, index, value):
         if index >= len(self.sub_range_list):
             return
         self.sub_range_list[index].set_equity(value)
-        
+
     def set_summary_equity(self, value):
         if value < 0 or value > 100:
             self.summary_eq.set("??.?")
         else:
-            self.summary_eq.set("%.1f" % value)
+            self.summary_eq.set(f"{value:.1f}")
         return
-    
+
     def set_range_equity(self, value):
         if value < 0 or value > 100:
             self.range_eq.set("??.?")
         else:
-            self.range_eq.set("%.1f" % value)
+            self.range_eq.set(f"{value:.1f}")
         return
 
-    def add_parenthesis(self, range_string):
-        if '+' in range_string:
-           return "(" + range_string + ")" 
-        if len(range_string) < 3:
-            return range_string        
-        if range_string[0]=='(' and range_string[-1] == ')':
-            return range_string
-        if ',' in range_string or '!' in range_string or ':' in range_string:
-            return "(" + range_string + ")"
-        return range_string
-    
-    def get_selected_range(self): # return selected range combined with start range
-        selected_range=""
-        for i in range (0, len(self.sub_range_list)):
-            if self.get_xbox(i):
-                if self.get_range(i,False):
-                    selected_range=selected_range+","+self.get_range(i,False)
-        if selected_range:
-            return self.add_parenthesis(self.get_start_range()) + ":" +self.add_parenthesis(selected_range[1:])
+
+    def get_selected_range(self):
+        selected_parts = []
+        for i in range(len(self.sub_range_list)):
+            if self.get_xbox(i) and self.sub_range_list[i].get_range():
+                selected_parts.append(self.sub_range_list[i].get_range())
+        if selected_parts:
+            selected_range = ",".join(selected_parts)
+            # Ajout conditionnel des parenthèses sur chaque partie
+            return f"{self.add_parenthesis(self.get_start_range())}:{self.add_parenthesis(selected_range)}"
         else:
             return self.get_start_range()
+
 
     def get_certain_range(self, index=[]): # return ranges listed in index list (0-3) with start range; empty string if index list or ranges are empty
         selected_range=""
@@ -242,7 +261,7 @@ class Range:
             return self.add_parenthesis(self.get_start_range()) + ":" +self.add_parenthesis(selected_range[1:])
         else:
             return ""
-        
+
     def get_range(self, index, ignore_selection=True, start_range=False): # remove ranges listed above index
                                                                           # ignore_selection == False -> Doesnt exclude selected ranges
         if index >= len(self.sub_range_list):
@@ -253,7 +272,7 @@ class Range:
             if sub_range:
                 if ignore_selection or not self.get_xbox(i):
                     exclude_range=exclude_range + sub_range + ","
-                    
+
         include_range=self.add_parenthesis(self.sub_range_list[index].get_range())
 
         if exclude_range:
@@ -274,18 +293,18 @@ class Range:
             else:
                 return ""
 
-            
 
-class RangePreflop:
+
+class RangePreflop(RangeFormatterMixin):
     def __init__(self, master, player="Player", text_field_length=INPUT_LENGTH):
         self.title=ttk.Label(master, text=player)
-        self.title.grid(column=0, row=0, sticky=W)       
+        self.title.grid(column=0, row=0, sticky=W)
 
-        self.include_range=StringVar();
+        self.include_range=StringVar()
         self.include_range.set("")
-        self.exclude_range=StringVar();
+        self.exclude_range=StringVar()
         self.exclude_range.set("")
-        
+
         self.input_range1=ttk.Entry(master,textvariable=self.include_range,width=text_field_length,font=(FONT_FAM,FONT_SIZE))
         self.input_range1.grid(column=0, row=1, sticky=W)
 
@@ -293,18 +312,11 @@ class RangePreflop:
 
         self.input_range2=ttk.Entry(master,textvariable=self.exclude_range,width=text_field_length//2,font=(FONT_FAM,FONT_SIZE))
         self.input_range2.grid(column=2, row=1, sticky=W)
- 
-        for element in master.winfo_children(): element.grid_configure(padx=PADX, pady=PADY)       
 
-    def add_parenthesis(self, range_string):
-        if len(range_string) < 3:
-            return range_string        
-        if range_string[0]=='(' and range_string[-1] == ')':
-            return range_string
-        if ',' in range_string or '!' in range_string or ':' in range_string:
-            return "(" + range_string + ")"
-        return range_string
-        
+        for element in master.winfo_children():
+            element.grid_configure(padx=PADX, pady=PADY)
+
+
     def get_range(self):
         if not self.include_range.get():
             return ""
@@ -324,52 +336,6 @@ class EvCalcPlayer:
         self.pre=RangePreflop(self.preframe,player,pre_field_length)
         self.post=Range(self.playerframe,"Range")
 
-        
-        
-def test_button(flop_range):
-    print("HURRAY")
-#    print(flop_range.get_range(3,False))
-#    print(flop_range.get_selected_range())
-    print(flop_range.get_range())
-    return
-    
-def test():
-    root = Tk()
-    style = ttk.Style()
-    style.configure('.', font=(FONT_FAM, FONT_SIZE))
 
-    root.title("GUI Element Test")
-    root.geometry("1200x1000")
-    # range_line=RangeLine(root)
-    # range_line.set_freq(145)
-    # range_line.set_equity(34.4958685)
-    
-    frame_0=ttk.Frame(root, padding=FRAME_PADDING)
-    frame_0.grid(column=0, row=0, sticky=W)
-
-    frame_1=ttk.Frame(root, padding=FRAME_PADDING)
-    frame_1.grid(column=0, row=1, sticky=W)
-
-    frame_2=ttk.Frame(root, padding=FRAME_PADDING)
-    frame_2.grid(column=0, row=2, sticky=W)
-
-    pre_range=RangePreflop(frame_0,"Player 1",INPUT_LENGTH-10)
-    flop_range=Range(frame_1,"FLOP", "10%")
-    flop_range.set_freq(3,34)
-    flop_range.set_freq(1,-34)    
-    flop_range.set_freq(0,23.4569809845)
-    flop_range.set_equity(0,324)
-    flop_range.set_equity(2,32)    
-    flop_range.set_equity(4,23.4569809845)
-
-    test_bu=ttk.Button(frame_2, text="DO TEST", command= lambda: test_button(pre_range))
-    test_bu.grid(column=0, row=0, sticky=W)
-    
-    root.mainloop()
-    
-if __name__ == '__main__':
-    import timeit
-    test()
-
-# 
+#
 # gui_elements.py ends here
